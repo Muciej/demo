b5d81140099e2af768be54ef646e28110231a3a3
Muciej <maciej.jozefkowicz@gmail.com>
2021-10-09 13:37:21 +0200

    updated main.java

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static main(String args[]){
-
+        System.out.println("Zmodyfikowano");
+        for(int i=0; i<5; i++) System.out.println("Kurdebela");
     }
 }
